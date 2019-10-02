c19fbd98701cbd9ca1c1e414533c15bba6a828cb
4n0nD3v3l0p3r <56003470+4n0nD3v3l0p3r@users.noreply.github.com>
2019-10-02 09:45:42 +0100

    Create sky_remote.py

---
+++ sky_remote/sky_remote.py
@@ -0,0 +1,135 @@
+import time, math, socket, struct, time
+import requests
+import json
+
+ROOT = "/home/homeassistant/.homeassistant/"
+
+class SkyRemote:
+    commands={"power": 0, "select": 1, "backup": 2, "dismiss": 2, "channelup": 6, "channeldown": 7, "interactive": 8, "sidebar": 8, "help": 9, "services": 10, "search": 10, "tvguide": 11, "home": 11, "i": 14, "text": 15,  "up": 16, "down": 17, "left": 18, "right": 19, "red": 32, "green": 33, "yellow": 34, "blue": 35, "0": 48, "1": 49, "2": 50, "3": 51, "4": 52, "5": 53, "6": 54, "7": 55, "8": 56, "9": 57, "play": 64, "pause": 65, "stop": 66, "record": 67, "fastforward": 69, "rewind": 71, "boxoffice": 240, "sky": 241}
+    connectTimeout = 1000
+
+    def __init__(self, ip, port=49160):
+        self.ip=ip
+        self.port=port
+
+    def http_json(self) -> str:
+        response = requests.get('http://' + self.ip + ':9006/as/system/information')
+        return response.content
+
+    def powerStatus(self) -> str:
+        output = json.loads(self.http_json())        
+        if (output['activeStandby'] == False):
+            return 'On'
+        else:
+            return 'Off'
+    
+    
+    def press(self, sequence):
+        if isinstance(sequence, list):
+            for item in sequence:
+                if item not in self.commands:
+                    print('Invalid command: {}'.format(item))
+                    break
+                self.sendCommand(self.commands[item.casefold()])
+                time.sleep(0.5)
+        else:
+            if sequence not in self.commands:
+                print('Invalid command: {}'.format(sequence))
+            else:
+                self.sendCommand(self.commands[sequence])    
+
+    def sendCommand(self, code):
+        commandBytes = bytearray([4,1,0,0,0,0, int(math.floor(224 + (code/16))), code % 16])
+
+        try:
+            client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except socket.error as msg:
+            print('Failed to create socket. Error code: %s , Error message : %s' % (str(msg[0]), msg[1]))
+            return
+
+        try:
+            client.connect((self.ip, self.port))
+        except:
+            print("Failed to connect to client")
+            return
+
+        l=12
+        timeout=time.time()+self.connectTimeout
+
+        while 1:
+            data=client.recv(1024)
+            data=data
+
+            if len(data)<24:
+                client.sendall(data[0:l])
+                l=1
+            else:
+                client.sendall(commandBytes)
+                commandBytes[1]=0
+                client.sendall(commandBytes)
+                client.close()
+                break
+
+            if time.time() > timeout:
+                print("timeout error")
+                break
+            
+
+class SwitchMaker:
+
+    def __init__(self, name, room, conf_folder):
+        self._name = name
+        self._room = room
+        ROOT = conf_folder
+        self._f= open(ROOT + "skyq" + self._room.replace(" ","") + ".yaml","w+")
+        self._f.write("    skyq_pause" + self._room.replace(" ","").lower() + ":\n" + 
+                "      value_template: '{{\"off\"}}'\n" + 
+                "      friendly_name: 'pause in the " + self._room + "'\n" + 
+                "      turn_on:\n" + 
+                "        service: media_player.media_pause\n" + 
+                "        data:\n" + 
+                "          entity_id: media_player." + self._name.replace(" ","_").lower() + "\n" +
+                "      turn_off:\n" + 
+                "        service: script.placeholder\n")
+        self._f.write("    skyq_play" + self._room.replace(" ","").lower() + ":\n" + 
+                "      value_template: '{{\"off\"}}'\n" + 
+                "      friendly_name: 'play in the " + self._room + "'\n" + 
+                "      turn_on:\n" + 
+                "        service: media_player.media_play\n" + 
+                "        data:\n" + 
+                "          entity_id: media_player." + self._name.replace(" ","_").lower() + "\n" +
+                "      turn_off:\n" + 
+                "        service: script.placeholder\n")
+        self._f.write("    skyq_ff" + self._room.replace(" ","").lower() + ":\n" + 
+                "      value_template: '{{\"off\"}}'\n" + 
+                "      friendly_name: 'fastforward in the " + self._room + "'\n" + 
+                "      turn_on:\n" + 
+                "        service: media_player.media_next_track\n" + 
+                "        data:\n" + 
+                "          entity_id: media_player." + self._name.replace(" ","_").lower() + "\n" +
+                "      turn_off:\n" + 
+                "        service: script.placeholder\n")
+        self._f.write("    skyq_rw" + self._room.replace(" ","").lower() + ":\n" + 
+                "      value_template: '{{\"off\"}}'\n" + 
+                "      friendly_name: 'rewind in the " + self._room + "'\n" + 
+                "      turn_on:\n" + 
+                "        service: media_player.media_previous_track\n" + 
+                "        data:\n" + 
+                "          entity_id: media_player." + self._name.replace(" ","_").lower() + "\n" +
+                "      turn_off:\n" + 
+                "        service: script.placeholder\n")
+        
+    def addChannel(self, channel):
+        self._f.write("    skyq_" + channel.replace(" ","").lower() + self._room.replace(" ","").lower() + ":\n" + 
+                "      value_template: '{{\"off\"}}'\n" + 
+                "      friendly_name: '"+ channel + " in the " + self._room + "'\n" + 
+                "      turn_on:\n" + 
+                "        service: media_player.select_source\n" + 
+                "        data:\n" + 
+                "          entity_id: media_player." + self._name.replace(" ","_").lower() + "\n" +
+                "          source: '" + channel + "'\n" +  
+                "      turn_off:\n" + 
+                "        service: script.placeholder\n")
+    def closeFile(self):
+        self._f.close()
+           
