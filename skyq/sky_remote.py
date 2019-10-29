2ca57068aafadffb5c2b92d623b29bb124cdb525
MadManMonty <MadmanMonty@users.noreply.github.com>
2019-10-29 16:20:05 +0000

    Added connection check, fixes #1
    Sets connection timeout and handles connection failures for powerStatus checks

--- skyq/sky_remote.py
+++ skyq/sky_remote.py
@@ -5,6 +5,7 @@ import json
 class SkyRemote:
     commands={"power": 0, "select": 1, "backup": 2, "dismiss": 2, "channelup": 6, "channeldown": 7, "interactive": 8, "sidebar": 8, "help": 9, "services": 10, "search": 10, "tvguide": 11, "home": 11, "i": 14, "text": 15,  "up": 16, "down": 17, "left": 18, "right": 19, "red": 32, "green": 33, "yellow": 34, "blue": 35, "0": 48, "1": 49, "2": 50, "3": 51, "4": 52, "5": 53, "6": 54, "7": 55, "8": 56, "9": 57, "play": 64, "pause": 65, "stop": 66, "record": 67, "fastforward": 69, "rewind": 71, "boxoffice": 240, "sky": 241}
     connectTimeout = 1000
+    TIMEOUT = 3
 
     REST_BASE_URL = 'http://{0}:{1}/as/{2}'
     REST_PATH_INFO = 'system/information'
@@ -15,12 +16,15 @@ class SkyRemote:
         self._jsonport = jsonport
 
     def http_json(self, path) -> str:
-        response = requests.get(self.REST_BASE_URL.format(self._host, self._jsonport, path))
-        return response.content
+        try:
+            response = requests.get(self.REST_BASE_URL.format(self._host, self._jsonport, path), timeout=self.TIMEOUT)
+            return json.loads(response.content)
+        except Exception as err:
+            return {}
 
     def powerStatus(self) -> str:
-        output = json.loads(self.http_json(self.REST_PATH_INFO))        
-        if (output['activeStandby'] == False):
+        output = self.http_json(self.REST_PATH_INFO)
+        if ('activeStandby' in output and output['activeStandby'] == False):
             return 'On'
         else:
             return 'Off'
