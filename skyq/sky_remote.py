110764660719a8f526e5220fe66c9f12e350f745
Roger Selwyn <roger@selwyn.org.uk>
2020-01-09 13:34:39 +0000

    Images from HTTPS and reduce timeout

--- skyq/sky_remote.py
+++ skyq/sky_remote.py
@@ -34,7 +34,7 @@ REST_RECORDING_DETAILS = 'pvr/details/{0}'
 # Sky specific constants
 CURRENT_URI = 'CurrentURI'
 CURRENT_TRANSPORT_STATE = 'CurrentTransportState'
-IMAGE_URL_BASE = 'http://images.metadata.sky.com/pd-image/{0}/16-9/1788'
+IMAGE_URL_BASE = 'https://images.metadata.sky.com/pd-image/{0}/16-9/1788'
 PVR = 'pvr'
 XSI = 'xsi'
 
@@ -42,7 +42,7 @@ XSI = 'xsi'
 class SkyRemote:
     commands={"power": 0, "select": 1, "backup": 2, "dismiss": 2, "channelup": 6, "channeldown": 7, "interactive": 8, "sidebar": 8, "help": 9, "services": 10, "search": 10, "tvguide": 11, "home": 11, "i": 14, "text": 15,  "up": 16, "down": 17, "left": 18, "right": 19, "red": 32, "green": 33, "yellow": 34, "blue": 35, "0": 48, "1": 49, "2": 50, "3": 51, "4": 52, "5": 53, "6": 54, "7": 55, "8": 56, "9": 57, "play": 64, "pause": 65, "stop": 66, "record": 67, "fastforward": 69, "rewind": 71, "boxoffice": 240, "sky": 241}
     connectTimeout = 1000
-    TIMEOUT = 3
+    TIMEOUT = 2
 
     REST_BASE_URL = 'http://{0}:{1}/as/{2}'
     REST_PATH_INFO = 'system/information'
