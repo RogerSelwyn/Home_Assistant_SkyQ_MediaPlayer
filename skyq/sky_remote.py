383c539654bcd7f1643b7e38da0da15fb5ef8afa
Roger Selwyn <roger@selwyn.org.uk>
2020-03-07 09:05:56 +0000

    Change feed

--- skyq/sky_remote.py
+++ skyq/sky_remote.py
@@ -49,7 +49,7 @@ PVR = 'pvr'
 XSI = 'xsi'
 
 xmlTvUrlBase = 'http://www.xmltv.co.uk'
-xmlTvUrl = xmlTvUrlBase + '/feed/6743'
+xmlTvUrl = xmlTvUrlBase + '/feed/3364'
 
 class SkyRemote:
     commands={"power": 0, "select": 1, "backup": 2, "dismiss": 2, "channelup": 6, "channeldown": 7, "interactive": 8, "sidebar": 8, "help": 9, "services": 10, "search": 10, "tvguide": 11, "home": 11, "i": 14, "text": 15,  "up": 16, "down": 17, "left": 18, "right": 19, "red": 32, "green": 33, "yellow": 34, "blue": 35, "0": 48, "1": 49, "2": 50, "3": 51, "4": 52, "5": 53, "6": 54, "7": 55, "8": 56, "9": 57, "play": 64, "pause": 65, "stop": 66, "record": 67, "fastforward": 69, "rewind": 71, "boxoffice": 240, "sky": 241}
