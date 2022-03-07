b4d8f6a1ffa4f79b97af791e4f13de6dfd83f5ab
RogerSelwyn <rogers@selwyn.org.uk>
2022-03-07 11:13:07 +0000

    Send 'pause' when not on application

--- custom_components/skyq/media_player.py
+++ custom_components/skyq/media_player.py
@@ -399,7 +399,14 @@ class SkyQDevice(SkyQEntity, MediaPlayerEntity):
 
     async def async_media_pause(self):
         """Pause the current media item."""
-        await self.hass.async_add_executor_job(self._remote.press, "play")
+        app = await self.hass.async_add_executor_job(
+            self._remote.get_active_application
+        )
+        if app.appId == APP_EPG:
+            await self.hass.async_add_executor_job(self._remote.press, "pause")
+        else:
+            await self.hass.async_add_executor_job(self._remote.press, "play")
+
         self._state = STATE_PAUSED
         self.async_write_ha_state()
 
